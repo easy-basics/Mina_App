81e0a709c3ed68ce8f49138904154682b49ff19f
dengtongjing <dengtongjing@crland.com.cn>
2026-05-18 18:57:50 +0800

    fix：update img

--- admin/src/api/upload.js
+++ admin/src/api/upload.js
@@ -1,20 +1,10 @@
-import axios from 'axios'
+import request from '@/utils/request'
 
 export function uploadFile(file) {
   const formData = new FormData()
   formData.append('file', file)
-  const token = sessionStorage.getItem('token')
-  return axios
-    .post('/api/upload', formData, {
-      headers: {
-        Authorization: token ? `Bearer ${token}` : '',
-        'Content-Type': 'multipart/form-data',
-      },
-    })
-    .then((res) => {
-      if (res.data.code !== 0) {
-        return Promise.reject(new Error(res.data.message))
-      }
-      return res.data
-    })
+
+  return request.post('/upload', formData, {
+    headers: { 'Content-Type': 'multipart/form-data' },
+  })
 }
